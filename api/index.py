e5c93eb9d251f7e4b1b192be1e7fb33f37126cee
ggomez0 <gaspargomez2000@outlook.com>
2025-06-02 00:22:17 -0300

    switch exchange from Binance to Kraken

--- api/index.py
+++ api/index.py
@@ -1,17 +1,9 @@
-from dotenv import load_dotenv
 from flask import Flask
 import ccxt
-import os
-
-API_KEY = os.getenv('API_KEY')
-API_SECRET = os.getenv('API_SECRET')
-load_dotenv()
 
 app = Flask(__name__)
 
-exchange = ccxt.binance({
-    'apiKey': API_KEY,
-    'secret': API_SECRET,
+exchange = ccxt.kraken({
     'enableRateLimit': True,
     'options': {
         'defaultType': 'spot',
